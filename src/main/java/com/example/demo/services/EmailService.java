801e530b2cba6ef23185de74e02b5cbebc6074e5
Fabrizio Abreu <fabrizioabreu@live.com>
2021-10-29 00:11:50 -0300

    Padrão de projeto Strategy para usar mais de um EmailService

--- src/main/java/com/example/demo/services/EmailService.java
+++ src/main/java/com/example/demo/services/EmailService.java
@@ -1,48 +1,8 @@
 package com.example.demo.services;
 
-import java.io.IOException;
-
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Service;
 import com.example.demo.dto.EmailDTO;
-import com.example.demo.services.exceptions.EmailException;
-import com.sendgrid.Method;
-import com.sendgrid.Request;
-import com.sendgrid.Response;
-import com.sendgrid.SendGrid;
-import com.sendgrid.helpers.mail.Mail;
-import com.sendgrid.helpers.mail.objects.Content;
-import com.sendgrid.helpers.mail.objects.Email;
 
-@Service
-public class EmailService {
-	
-	private static Logger LOG = LoggerFactory.getLogger(EmailService.class);
+public interface EmailService {
 
-	@Autowired
-	private SendGrid sendGrid;
-	
-	public void sendEmail(EmailDTO dto) {
-		Email from = new Email(dto.getFromEmail(), dto.getFromName());
-		Email to   = new Email(dto.getTo());
-		Content content = new Content(dto.getContentType(), dto.getBody());
-		Mail mail = new Mail(from, dto.getSubject(), to, content);		
-		Request request = new Request();		
-		try {
-			request.setMethod(Method.POST);
-			request.setEndpoint("mail/send");
-			request.setBody(mail.build());			
-			LOG.info("Enviando email para: " + dto.getTo());			
-			Response response = sendGrid.api(request);			
-			if (response.getStatusCode() >= 400 && response.getStatusCode() <= 500) {
-				LOG.error("Erro ao enviar email: " + response.getBody());
-				throw new EmailException(response.getBody());
-			}
-				LOG.info("Email enviado! Status = " + response.getStatusCode());
-		} catch (IOException e) {
-			throw new EmailException(e.getMessage());
-		}
-	}
+	void sendEmail(EmailDTO dto);
 }
